adfba927b7bdc75e48c5b9135999f21e859fd678
jonatan <jonathan.leon.caparros@gmail.com>
2022-10-17 13:09:03 +0200

    commit de rest-assured

--- src/test/java/com/example/demo/RestAssuredApplicationTests.java
+++ src/test/java/com/example/demo/RestAssuredApplicationTests.java
@@ -10,27 +10,31 @@ import static io.restassured.RestAssured.*;
 @SpringBootTest
 class RestAssuredApplicationTests {
 
+//	@Test
+//	void contextLoads() {
+//		RestAssured.baseURI = "https://rahulshettyacademy.com";
+//		given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+//		.body("{\n"
+//				+ "    \"location\": {\n"
+//				+ "        \"lat\": -38.383494,\n"
+//				+ "        \"lng\": 33.427362\n"
+//				+ "    },\n"
+//				+ "    \"accuracy\": 50,\n"
+//				+ "    \"name\": \"Rahul Shetty Academy\",\n"
+//				+ "    \"phone_number\": \"(+91) 983 893 3937\",\n"
+//				+ "    \"adress\": \"29, side layout, cohen 09\",\n"
+//				+ "    \"types\": [\n"
+//				+ "        \"shoe park\",\n"
+//				+ "        \"shop\"\n"
+//				+ "    ],\n"
+//				+ "    \"website\": \"https://rahulshettyacademy.com\",\n"
+//				+ "    \"language\": \"French-IN\"\n"
+//				+ "}").when().post("maps/api/place/add/json")
+//		.then().assertThat().statusCode(200);
+//	}
 	@Test
-	void contextLoads() {
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\n"
-				+ "    \"location\": {\n"
-				+ "        \"lat\": -38.383494,\n"
-				+ "        \"lng\": 33.427362\n"
-				+ "    },\n"
-				+ "    \"accuracy\": 50,\n"
-				+ "    \"name\": \"Rahul Shetty Academy\",\n"
-				+ "    \"phone_number\": \"(+91) 983 893 3937\",\n"
-				+ "    \"adress\": \"29, side layout, cohen 09\",\n"
-				+ "    \"types\": [\n"
-				+ "        \"shoe park\",\n"
-				+ "        \"shop\"\n"
-				+ "    ],\n"
-				+ "    \"website\": \"https://rahulshettyacademy.com\",\n"
-				+ "    \"language\": \"French-IN\"\n"
-				+ "}").when().post("maps/api/place/add/json")
-		.then().assertThat().statusCode(200);
+	void postJira() {
+		System.out.println("Post Jira");
 	}
 
 }
